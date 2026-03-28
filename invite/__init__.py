b22085eca22b7740270ea1eafc5a404ebb59a109
Julio Conchas <jconchas@ibm.com>
2026-03-28 13:36:31 -0600

    adding styles and config to get hero image from tigris bucket and easier setup

--- invite/__init__.py
+++ invite/__init__.py
@@ -1,10 +1,25 @@
 #wedding-ivite/invite/__init__.py
 #issue  dev           date       description
 # na    Julio Conchas 12/29/2025 first creation
+# na    Julio Conchas 03/28/2026 Adding bucked name and url
+
+from dotenv import load_dotenv
+load_dotenv()
 
 from flask import Flask
 from invite.core.view import core
+import os
 
 app = Flask(__name__)
 
+app.config['TIGRIS_BASE_URL'] = os.getenv('BUCKET_NAME')
 app.register_blueprint(core)
+
+##############
+# app config #
+##############
+@app.context_processor
+def inject_globals():
+    return {
+        'MEDIA_URL': app.config.get('TIGRIS_BASE_URL')
+    }
--- invite/core/view.py
+++ invite/core/view.py
@@ -1,9 +1,10 @@
-#wedding-ivite/invite/view.py
+#wedding-ivite/invite/core/view.py
 #issue  dev           date       description
 # na    Julio Conchas 12/29/2025 first creation
 # na    Julio Conchas 01/02/2026  add test view
+# na    Julio Conchas 03/28/2026 Adding endpoint for favicon.ico
 
-from flask import render_template, Blueprint
+from flask import render_template, Blueprint, send_from_directory
 
 core = Blueprint('core',__name__)
 
@@ -14,3 +15,7 @@ def index():
 @core.route('/test')
 def test():
     return render_template('test.html')
+
+@core.route('/favicon.ico')
+def favicon():
+    return send_from_directory('static', 'img/icon/favicon.ico')
