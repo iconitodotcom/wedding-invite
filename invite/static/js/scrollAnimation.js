e36885f69c5a85f396bc51f08799ccf9b7e403ab
Julio Conchas <jconchas@ibm.com>
2026-03-31 09:25:01 -0600

    detail section, adding parents, and godfathers

--- invite/static/js/scrollAnimation.js
+++ invite/static/js/scrollAnimation.js
@@ -2,17 +2,12 @@
 const observer = new IntersectionObserver((entries) => {
     entries.forEach(entry => {
       if (entry.isIntersecting) {
-        if (entry.target.classList.contains('reveal-line')) { entry.target.classList.add('visible') }
+        if (entry.target.matches('.reveal-line, .reveal-left, .reveal-right')) { entry.target.classList.add('visible') }
         if (entry.target.classList.contains('typewriter')) { entry.target.classList.add('animate') }
         //entry.target.classList.add('animate');
         observer.unobserve(entry.target);  // only animate once
       }
     });
-  }, { threshold: 1 });  // trigger when 30% visible
+  }, { threshold: 0.3 });  // trigger when 30% visible
   
-  document.querySelectorAll('.typewriter').forEach(el => observer.observe(el));
-  document.querySelectorAll('.reveal-line').forEach((el, i ) => {
-    // stagger each line
-   // el.querySelector('span').style.transitionDelay = `${i * 0.3}s`;
-    observer.observe(el);
-  });
+  document.querySelectorAll('.typewriter, .reveal-line, .reveal-right, .reveal-left').forEach(el => observer.observe(el));
