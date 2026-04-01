1b620b1b05562b08470b2499bf712bb680d5e500
Julio Conchas <jconchas@ibm.com>
2026-04-01 11:17:09 -0600

    adding image after detail section

--- invite/static/js/scrollAnimation.js
+++ invite/static/js/scrollAnimation.js
@@ -4,10 +4,11 @@ const observer = new IntersectionObserver((entries) => {
       if (entry.isIntersecting) {
         if (entry.target.matches('.reveal-line, .reveal-left, .reveal-right')) { entry.target.classList.add('visible') }
         if (entry.target.classList.contains('typewriter')) { entry.target.classList.add('animate') }
+        if (entry.target.classList.contains('curtain-wrapper')) { entry.target.classList.add('revealed') }
         //entry.target.classList.add('animate');
         observer.unobserve(entry.target);  // only animate once
       }
     });
   }, { threshold: 0.3 });  // trigger when 30% visible
   
-  document.querySelectorAll('.typewriter, .reveal-line, .reveal-right, .reveal-left').forEach(el => observer.observe(el));
+  document.querySelectorAll('.typewriter, .reveal-line, .reveal-right, .reveal-left, .curtain-wrapper').forEach(el => observer.observe(el));
