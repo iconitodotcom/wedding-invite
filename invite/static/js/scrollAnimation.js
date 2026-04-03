8379e20a7237c3ad72d46034ab7aaf08b87b8bf9
Julio Conchas <jconchas@ibm.com>
2026-04-03 13:58:21 -0600

    #6 venue image with slide Overshoot effect added

--- invite/static/js/scrollAnimation.js
+++ invite/static/js/scrollAnimation.js
@@ -6,10 +6,11 @@ const observer = new IntersectionObserver((entries) => {
         if (entry.target.classList.contains('typewriter')) { entry.target.classList.add('animate') }
         if (entry.target.classList.contains('curtain-wrapper')) { entry.target.classList.add('revealed') }
         if (entry.target.classList.contains('fade-in')) { entry.target.classList.add('show') }
+        if (entry.target.classList.contains('fancy-image')) { entry.target.classList.add('fshow') }
         //entry.target.classList.add('animate');
         observer.unobserve(entry.target);  // only animate once
       }
     });
   }, { threshold: 0.3 });  // trigger when 30% visible
   
-  document.querySelectorAll('.typewriter, .reveal-line, .reveal-right, .reveal-left, .curtain-wrapper, .fade-in').forEach(el => observer.observe(el));
+  document.querySelectorAll('.typewriter, .reveal-line, .reveal-right, .reveal-left, .curtain-wrapper, .fade-in, .fancy-image').forEach(el => observer.observe(el));
