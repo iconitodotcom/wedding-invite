cf9f36a0bc566b3e46780b6a48dbd051cec9b3a5
Julio Conchas <jconchas@ibm.com>
2026-05-19 16:04:53 -0600

    #8 adding program section phase I

--- invite/static/js/scrollAnimation.js
+++ invite/static/js/scrollAnimation.js
@@ -7,10 +7,11 @@ const observer = new IntersectionObserver((entries) => {
         if (entry.target.classList.contains('curtain-wrapper')) { entry.target.classList.add('revealed') }
         if (entry.target.classList.contains('fade-in')) { entry.target.classList.add('show') }
         if (entry.target.classList.contains('fancy-image')) { entry.target.classList.add('fshow') }
+        if (entry.target.classList.contains('tl-item')) { entry.target.classList.add('visible-item')}
         //entry.target.classList.add('animate');
         observer.unobserve(entry.target);  // only animate once
       }
     });
   }, { threshold: 0.3 });  // trigger when 30% visible
   
-  document.querySelectorAll('.typewriter, .reveal-line, .reveal-right, .reveal-left, .curtain-wrapper, .fade-in, .fancy-image').forEach(el => observer.observe(el));
+  document.querySelectorAll('.typewriter, .reveal-line, .reveal-right, .reveal-left, .curtain-wrapper, .fade-in, .fancy-image, .tl-item').forEach(el => observer.observe(el));
